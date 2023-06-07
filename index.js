97138e024b00323b256055511748f2216dd25ba0
Taha <tahaozcilingir@msn.com>
2023-06-07 17:50:32 -0400

    DOM Modifying

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.head.appendChild(element);
